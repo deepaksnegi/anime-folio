17566e7edb1b510af0c4556e3332c5717f76cfcf
deepaksnegi <deepaksnegi16@gmail.com>
2024-03-21 22:54:01 +0530

    remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-// min and max included
-export function randomIntFromInterval(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
