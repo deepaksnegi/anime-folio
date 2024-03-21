47d7ccda47bad9b014ae6ce388ac03aeb5b9d8b7
deepaksnegi <deepaksnegi16@gmail.com>
2024-03-21 22:46:23 +0530

    added large image support

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -21,72 +21,86 @@ import { AnimeDetails } from "@/types/ApiResponse";
 type Props = {};
 
 const Hero = ({ animeList }: { animeList: AnimeDetails[] }) => {
-  const heroSlide = animeList.map((anime) => (
-    <CarouselItem data-bs-interval="4 00" key={anime.mal_id}>
-      <div className="h-[55vh] min-h-72 w-full cursor-pointer">
-        <div className="relative flex h-full">
-          <div className="w-[30%]">
-            <div className="absolute bottom-12 left-6 z-10 w-[40%] space-y-4 md:left-12">
-              <span className="text-lg font-semibold">
-                #{anime.rank} Spotlight
-              </span>
-              <h1 className="text-xl font-medium lg:text-4xl">
-                {anime.title_english}
-              </h1>
-              <div className="hidden gap-10 md:flex">
-                <span className="flex items-center gap-1">
-                  <CalendarIcon size={16} /> {anime.year}
-                </span>
-                <span className="flex items-center gap-1">
-                  <PlayCircleIcon size={16} />
-                  {anime.episodes}
-                </span>
-                <span className="flex items-center gap-1">
-                  <ClockIcon size={16} />
-                  {anime.duration}
-                </span>
-                <span className="flex items-center gap-1">
-                  <Star size={16} className="text-green-400" />
-                  <span className="episode-count">{anime.score}</span>
-                </span>
-              </div>
-              <p className="hidden md:block">
-                {anime.synopsis?.slice(0, 200) + "..."}
-              </p>
-              <div className="flex gap-3">
-                <Link
-                  onClick={() => window.scrollTo({ top: 0 })}
-                  href={`/watch?name=`}
-                  className="flex h-11 items-center gap-x-2 rounded-3xl bg-secondary px-6 text-secondary-foreground hover:bg-secondary/80"
-                >
-                  Watch Now
-                  <PlayCircleIcon size={18} />
-                </Link>
-                <Link
-                  href={`/details/${anime.mal_id}/`}
-                  onClick={() => window.scrollTo({ top: 0 })}
-                  className="flex h-11 items-center gap-x-1 rounded-3xl bg-primary px-6 text-primary-foreground hover:bg-primary/90"
-                >
-                  Details <ChevronRight size={18} />
-                </Link>
+  const heroSlide = animeList.map(
+    ({
+      rank,
+      year,
+      episodes,
+      title_english,
+      duration,
+      score,
+      synopsis,
+      trailer,
+      mal_id,
+      images,
+    }) => (
+      <CarouselItem data-bs-interval="4 00" key={mal_id}>
+        <div className="h-[55vh] min-h-72 w-full cursor-pointer">
+          <div className="relative flex h-full">
+            <div className="w-[30%]">
+              <div className="absolute bottom-12 left-6 z-10 w-[40%] space-y-4 md:left-12">
+                <span className="text-lg font-semibold">#{rank} Spotlight</span>
+                <h1 className="text-xl font-medium lg:text-4xl">
+                  {title_english}
+                </h1>
+                <div className="hidden gap-10 md:flex">
+                  <span className="flex items-center gap-1">
+                    <CalendarIcon size={16} /> {year}
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <PlayCircleIcon size={16} />
+                    {episodes}
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <ClockIcon size={16} />
+                    {duration}
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <Star size={16} className="text-green-400" />
+                    <span className="episode-count">{score}</span>
+                  </span>
+                </div>
+                <p className="hidden md:block">
+                  {synopsis?.slice(0, 200) + "..."}
+                </p>
+                <div className="flex gap-3">
+                  <Link
+                    onClick={() => window.scrollTo({ top: 0 })}
+                    href={trailer.embed_url || `/details/${mal_id}/`}
+                    className="flex h-11 items-center gap-x-2 rounded-3xl bg-secondary px-6 text-secondary-foreground hover:bg-secondary/80"
+                  >
+                    Watch Now
+                    <PlayCircleIcon size={18} />
+                  </Link>
+                  <Link
+                    href={`/details/${mal_id}/`}
+                    onClick={() => window.scrollTo({ top: 0 })}
+                    className="flex h-11 items-center gap-x-1 rounded-3xl bg-primary px-6 text-primary-foreground hover:bg-primary/90"
+                  >
+                    Details <ChevronRight size={18} />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="relative w-[70%]">
-            <Image
-              className="object-cover"
-              src={anime.images?.webp.large_image_url}
-              fill
-              alt="anime"
-              priority
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-            <div className="absolute -left-1 h-full w-full bg-[linear-gradient(90deg,_#fff_5%,_#24242800_55%,_#24242800_10%)] dark:bg-[linear-gradient(90deg,_#020817_5%,_#24242800_55%,_#24242800_10%)]" />
+            <div className="relative w-[70%]">
+              <Image
+                className="object-cover"
+                src={
+                  trailer.images.maximum_image_url ??
+                  images.webp.large_image_url
+                }
+                fill
+                alt="anime"
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+              <div className="absolute -left-1 h-full w-full bg-[linear-gradient(90deg,_#fff_5%,_#24242800_55%,_#24242800_10%)] dark:bg-[linear-gradient(90deg,_#020817_5%,_#24242800_55%,_#24242800_10%)]" />
+            </div>
           </div>
         </div>
-      </div>
-    </CarouselItem>
-  ));
+      </CarouselItem>
+    ),
+  );
 
   return (
     <div className="relative">
