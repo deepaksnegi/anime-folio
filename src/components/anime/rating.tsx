1f373d4c258e407a54f3741ec312920af8c7e3df
deepaksnegi <deepaksnegi16@gmail.com>
2024-04-01 21:59:27 +0530

    added rating chart

--- src/components/anime/rating.tsx
+++ src/components/anime/rating.tsx
@@ -1,15 +1,17 @@
+import { Score } from "@/types/StatisticsResponse";
 import React from "react";
 
 type Props = {
+  scores: Score[];
   rating?: number;
   totalReviews?: number;
 };
 
 const Rating = (props: Props) => {
-  const { rating, totalReviews } = props;
+  const { rating, totalReviews, scores } = props;
   return (
-    <>
-      <div className="mb-5 flex items-center">
+    <div>
+      <div className="flex items-center">
         <p className="inline-flex items-center rounded bg-blue-100 p-1.5 text-sm font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800">
           {rating}
         </p>
@@ -21,125 +23,86 @@ const Rating = (props: Props) => {
           {totalReviews} reviews
         </p>
       </div>
-      <div className="gap-8 sm:grid sm:grid-cols-2">
-        <div>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Story
-            </dt>
-            <dd className="mb-3 flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 88%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                8.8
-              </span>
-            </dd>
-          </dl>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Animation
-            </dt>
-            <dd className="mb-3 flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 89%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                8.9
-              </span>
-            </dd>
-          </dl>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Free WiFi
-            </dt>
-            <dd className="mb-3 flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 88%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                8.8
-              </span>
-            </dd>
-          </dl>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Facilities
-            </dt>
-            <dd className="flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 54%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                5.4
-              </span>
-            </dd>
-          </dl>
-        </div>
-        <div>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Character Development
-            </dt>
-            <dd className="mb-3 flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 89%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                8.9
-              </span>
-            </dd>
-          </dl>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Cleanliness
-            </dt>
-            <dd className="mb-3 flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 70%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                7.0
-              </span>
-            </dd>
-          </dl>
-          <dl>
-            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-              Location
-            </dt>
-            <dd className="flex items-center">
-              <div className="me-2 h-2.5 w-full rounded bg-gray-200 dark:bg-gray-700">
-                <div
-                  className="h-2.5 rounded bg-blue-600 dark:bg-blue-500"
-                  // style="width: 89%"
-                ></div>
-              </div>
-              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                8.9
-              </span>
-            </dd>
-          </dl>
-        </div>
+
+      <div className="mb-2 flex items-center">
+        <svg
+          className="me-1 h-4 w-4 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="me-1 h-4 w-4 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="me-1 h-4 w-4 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="me-1 h-4 w-4 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="me-1 h-4 w-4 text-gray-300 dark:text-gray-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          4.95
+        </p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          out of
+        </p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          5
+        </p>
       </div>
-    </>
+      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+        {totalReviews} ratings
+      </p>
+      {scores.map(({ score, votes, percentage }) => (
+        <div className="mt-4 flex items-center" key={score}>
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            {score} star
+          </a>
+          <div className="mx-4 h-5 w-2/4 rounded bg-gray-200 dark:bg-gray-700">
+            <div
+              className="h-5 rounded bg-yellow-300"
+              style={{ width: `${percentage}%` }}
+            ></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            {percentage}% | {votes} votes
+          </span>
+        </div>
+      ))}
+    </div>
   );
 };
 
