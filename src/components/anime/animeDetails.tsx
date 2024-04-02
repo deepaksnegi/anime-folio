0ca4b3bf675382da1a0368ad9cf08d288dc56964
deepaksnegi <deepaksnegi16@gmail.com>
2024-04-02 21:13:59 +0530

    fixed dark theme issue

--- src/components/anime/animeDetails.tsx
+++ src/components/anime/animeDetails.tsx
@@ -70,53 +70,53 @@ const AnimeDetails = (props: Props) => {
 
       <div className="grid grid-cols-1 gap-y-4 px-4 md:grid-cols-3 md:gap-y-8 lg:grid-cols-4">
         <div>
-          <dt className="font-medium text-gray-900">Premiered</dt>
+          <dt className="font-medium">Premiered</dt>
           <dd className="text-gray-700 sm:col-span-2">
             {anime.season} | {anime.year}
           </dd>
         </div>
 
         <div>
-          <dt className="font-medium text-gray-900">Japanese Title</dt>
-          <dd className="text-gray-700 sm:col-span-2">
+          <dt className="font-medium">Japanese Title</dt>
+          <dd className="text-gray-500 sm:col-span-2">
             {anime.title_japanese}
           </dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Rating</dt>
-          <dd className="text-gray-700 sm:col-span-2">{anime.rating}</dd>
+          <dt className="font-medium">Rating</dt>
+          <dd className="text-gray-500 sm:col-span-2">{anime.rating}</dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Type</dt>
-          <dd className="text-gray-700 sm:col-span-2">{anime.type}</dd>
+          <dt className="font-medium">Type</dt>
+          <dd className="text-gray-500 sm:col-span-2">{anime.type}</dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Episodes</dt>
-          <dd className="text-gray-700 sm:col-span-2">{anime.episodes}</dd>
+          <dt className="font-medium">Episodes</dt>
+          <dd className="text-gray-500 sm:col-span-2">{anime.episodes}</dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Duration</dt>
-          <dd className="text-gray-700 sm:col-span-2">{anime.duration}</dd>
+          <dt className="font-medium">Duration</dt>
+          <dd className="text-gray-500 sm:col-span-2">{anime.duration}</dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Status</dt>
-          <dd className="text-gray-700 sm:col-span-2">{anime.status}</dd>
+          <dt className="font-medium">Status</dt>
+          <dd className="text-gray-500 sm:col-span-2">{anime.status}</dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Themes</dt>
-          <dd className="text-gray-700 sm:col-span-2">
+          <dt className="font-medium">Themes</dt>
+          <dd className="text-gray-500 sm:col-span-2">
             {anime.themes.map(({ name }) => name).join(", ")}
           </dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Built By</dt>
-          <dd className="text-gray-700 sm:col-span-2">
+          <dt className="font-medium">Built By</dt>
+          <dd className="text-gray-500 sm:col-span-2">
             {anime.studios.map((s) => s.name).join(", ")}
           </dd>
         </div>
         <div>
-          <dt className="font-medium text-gray-900">Also Known As</dt>
-          <dd className="text-gray-700 sm:col-span-2">
+          <dt className="font-medium">Also Known As</dt>
+          <dd className="text-gray-500 sm:col-span-2">
             {anime.title_synonyms.join(", ")}
           </dd>
         </div>
