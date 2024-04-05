372f070c83ca98ea0c42bf71add00109da845257
deepaksnegi <deepaksnegi16@gmail.com>
2024-04-05 22:33:25 +0530

    added loader and loading skeleton

--- src/components/loaders/loader.tsx
+++ src/components/loaders/loader.tsx
@@ -1,11 +1,16 @@
 import { LoaderIcon } from "lucide-react";
 import React from "react";
 
-const Loader = () => {
+interface Props {
+  showDialog?: boolean;
+}
+const Loader = ({ showDialog }: Props) => {
   return (
     <div className="flex h-full w-full  items-center justify-center gap-5">
       <LoaderIcon size={30} className="animate-spin " />
-      <h4>Loading... It&#39;s not a filler episode, we promise!</h4>
+      <h4>
+        Loading...{showDialog ? "It's not a filler episode, we promise!" : null}
+      </h4>
     </div>
   );
 };
